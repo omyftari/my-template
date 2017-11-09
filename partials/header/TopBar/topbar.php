0f803a510c6c9c8dd7a00dfbc64645db2c633442
ornela <ornelamyftari19@gmail.com>
2017-11-09 23:11:14 +0100

    initial commit

---
+++ partials/header/TopBar/topbar.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title> PSD TO HTML </title>
+    <link rel="stylesheet" href="assets/css/libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<div class="container">
+    <div class="row top-bar">
+        <div class="col-md-8 top-bar-text">
+            Tel: xxxxx xxxxxxxxxx | Mail: info@domain.com
+        </div>
+        <div class="col-md-3 search-bar">
+            <div class="row top-bar-rounded">
+                <div class="col-md-10 top-bar-search">
+                    SEARCH TERM HERE
+                </div>
+                <div class="col-md-2 top-bar-search ">
+                    <span class=" glyphicon glyphicon-search top-bar-icon"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row top-nav">
+
+        <div class="col-md-offset-3 col-md-8 ">
+            <nav class="navbar top-nav">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Homepage</a></li>
+                    <li><a href="#">Style Demo </a></li>
+                    <li><a href="#">Full width </a></li>
+                    <li><a href="#">Drop Down </a></li>
+                    <li><a href="#">Link Text </a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="row top-nav-bottom-border"></div>
+</div>
+</body>
