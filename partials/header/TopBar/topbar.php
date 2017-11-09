78b34c98189d8a05ff8b893b79f9f515740f992d
ornela <ornelamyftari19@gmail.com>
2017-11-09 23:12:21 +0100

    remove space

--- partials/header/TopBar/topbar.php
+++ partials/header/TopBar/topbar.php
@@ -23,7 +23,6 @@
     </div>
 
     <div class="row top-nav">
-
         <div class="col-md-offset-3 col-md-8 ">
             <nav class="navbar top-nav">
                 <ul class="nav navbar-nav">
